6148d69f59a90c45fdfb9901e25dbd788ce7cc16
ahthserhsluk <52717339+ahthserhsluk@users.noreply.github.com>
2020-10-06 14:36:55 +0530

    Update D03P2_LeetCode_1281.java

--- 200_LeetCode_Questions/Java/D03P2_LeetCode_1281.java
+++ 200_LeetCode_Questions/Java/D03P2_LeetCode_1281.java
@@ -1,17 +1,17 @@
-// Runtime: 0 ms, faster than 100.00% of Java online submissions for Subtract the Product and Sum of Digits of an Integer.
-// Memory Usage: 36.6 MB, less than 43.70% of Java online submissions for Subtract the Product and Sum of Digits of an Integer.
-
 class Solution {
-    public int subtractProductAndSum(int n) {
-        if(n == 0){
-            return 0;
-        }
-        int prod = 1, sum = 0;
-        while(n > 0){
-            prod *= n % 10;
-            sum += n % 10;
-            n /= 10;
-        }
-        return prod - sum;
+  public int subtractProductAndSum(int n) {
+    int sum = 0;
+    int copy = n;
+    while (copy > 0) {
+      sum += copy % 10;
+      copy /= 10;
     }
+    int prod = 1;
+    copy = n;
+    while (copy > 0) {
+      prod *= copy % 10;
+      copy /= 10;
+    }
+    return prod - sum;
+  }
 }
